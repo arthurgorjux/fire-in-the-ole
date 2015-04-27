6af31f67abb1ee0e341a2fd37a6ccfd1d020e132
arthurgorjux <arthurGorjux@gmail.com>
2015-04-27 13:21:43 +0000

    Netoyage code + refactoring position

--- Fire_In_The_Ole/src/Observer/Observateur.java
+++ Fire_In_The_Ole/src/Observer/Observateur.java
@@ -1,5 +1,12 @@
 package Observer;
 
+/**
+ * Interface utilisée pour l'implémentation du patron observateur.
+ * @author gael
+ */
 public interface Observateur {
-	public void prevenir();
+    /**
+     * Notifie l'observateur d'un évènement important.
+     */
+    public void prevenir();
 }
